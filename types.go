7dc3410100edc36930f63f43530829a388a3f503
Timo Beckers <timo@incline.eu>
2017-04-28 13:45:05 +0200

    conntrack: nearly-full protocol decode, error defs, pre-refactor

--- types.go
+++ types.go
@@ -1,13 +1,36 @@
 package netfilter
 
 import (
-	"github.com/mdlayher/netlink/nlenc"
-	"unsafe"
 	"errors"
+	"github.com/mdlayher/netlink/nlenc"
 )
 
 var (
 	errShortMessage = errors.New("cannot parse netfilter message because it is too short")
+)
+
+// Flag values for Netlink messages, taken from the 'unix' package.
+// From Linux/include/uapi/linux/netlink.h.
+const (
+	// Query types.
+	NLM_F_REQUEST       = 0x1
+	NLM_F_MULTI         = 0x2
+	NLM_F_ACK           = 0x4
+	NLM_F_ECHO          = 0x8
+	NLM_F_DUMP_INTR     = 0x10
+	NLM_F_DUMP_FILTERED = 0x20
+
+	// Modifiers to GET queries.
+	NLM_F_ROOT   = 0x100
+	NLM_F_MATCH  = 0x200
+	NLM_F_ATOMIC = 0x400
+	NLM_F_DUMP   = (NLM_F_ROOT | NLM_F_MATCH)
+
+	// Modifiers to NEW queries.
+	NLM_F_REPLACE = 0x100
+	NLM_F_EXCL    = 0x200
+	NLM_F_CREATE  = 0x400
+	NLM_F_APPEND  = 0x800
 )
 
 // Netfilter Subsystem ID definitions
@@ -37,7 +60,7 @@ const (
 type MessageType uint8
 
 // Size of a Netfilter header (4 Bytes)
-const nfHeaderLen = int(unsafe.Sizeof(Header{}))
+const nfHeaderLen = 4
 
 // The Netfilter Header type
 // Known in the Linux kernel sources as 'nfgenmsg' at Linux/include/uapi/linux/netfilter/nfnetlink.h
@@ -48,14 +71,14 @@ const nfHeaderLen = int(unsafe.Sizeof(Header{}))
 // |     ResourceID (2 Bytes)   |
 // ------------------------------
 type Header struct {
-	Family uint8
-	Version uint8
+	Family     uint8
+	Version    uint8
 	ResourceId uint16
 }
 
 // MarshalBinary marshals a Header into a byte slice.
 func (h *Header) MarshalBinary() ([]byte, error) {
-	b := make([]byte, 4)
+	b := make([]byte, nfHeaderLen)
 
 	b[0] = h.Family
 	b[1] = h.Version
