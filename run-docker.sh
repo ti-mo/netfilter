cce7d8c9171d755369ea7b36007e3a7418eb6f31
Timo Beckers <timo@incline.eu>
2017-05-12 14:53:02 +0200

    Improve Docker script with coverage report in browser

--- run-docker.sh
+++ run-docker.sh
@@ -7,6 +7,44 @@ cname="netfilter"
 cpath="/go/src/github.com/ti-mo/netfilter"
 dstatus=$(docker ps -a --format "{{.Names}} {{.Status}}")
 
+# --- Functions
+gocheck() {
+  if ! command -v go >/dev/null; then
+    echo "E: 'go' not installed on host, exiting."
+    exit
+  fi
+}
+
+usage() {
+  echo "$0 - run Docker development environment"
+  echo ""
+  echo "  :: This script sets up a Docker development environment."
+  echo "  :: By default, it runs the test suite of all nested packages."
+  echo ""
+  echo "Usage: $0 [-h] [-b]"
+  echo "    -h  This help."
+  echo "    -b  Open browser on the host with coverage report."
+  echo ""
+  exit 1
+}
+
+# --- Parse Arguments
+POST_BROWSER=""
+
+while getopts "hb" opt; do
+  case $opt in
+    b)
+      gocheck
+      POST_BROWSER="yes"
+      ;;
+    h)
+      usage
+      exit
+      ;;
+  esac
+done
+
+# --- Docker Instantiation
 if echo "$dstatus" | grep -q "^$cname[[:space:]]"; then
   if echo "$dstatus" | grep "^$cname[[:space:]]" | grep -q "Up"; then
     echo "Container '$cname' exists and is already running, exiting."
@@ -25,4 +63,10 @@ else
     golang "$cpath"/run-tests.sh
 
   echo "Container '$cname' terminated."
+fi
+
+# --- Post-run Tasks
+if [ ! -z "$POST_BROWSER" ]; then
+  echo "I: Opening browser with coverage report"
+  go tool cover -html=overalls.coverprofile
 fi
