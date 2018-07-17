9e919d577b0bd3542d4fe9286d2211dfa7a4f4e5
Timo Beckers <timo@incline.eu>
2018-07-17 20:24:18 +0200

    netfilter - import upstream netlink tests

--- attribute_test.go
+++ attribute_test.go
@@ -1,44 +1,205 @@
 package netfilter
 
 import (
+	"bytes"
 	"reflect"
 	"testing"
 
 	"github.com/mdlayher/netlink"
 )
 
-func TestHeader_MarshalUnmarshalMessage(t *testing.T) {
-	hdr := Header{
-		Family:     255,
-		Version:    1,
-		ResourceID: 2,
+func TestAttribute_Marshal(t *testing.T) {
+	tests := []struct {
+		name  string
+		attrs []Attribute
+		b     []byte
+		err   error
+	}{
+		{
+			name: "nested and endianness bits",
+			attrs: []Attribute{
+				{
+					Attribute: netlink.Attribute{
+						Data:   make([]byte, 0),
+						Length: 4,
+						Type:   0,
+					},
+					Nested:       true,
+					NetByteOrder: true,
+				},
+			},
+			err: errInvalidAttributeFlags,
+		},
+		{
+			name: "nested bit, type 1, length 0",
+			attrs: []Attribute{
+				{
+					Attribute: netlink.Attribute{
+						Length: 4,
+						Type:   1,
+						Data:   make([]byte, 0),
+					},
+					Nested: true,
+				},
+			},
+			b: []byte{
+				0x04, 0x00,
+				0x01, 0x80, // Nested bit
+			},
+		},
+		{
+			name: "endianness bit, type 1, length 0",
+			attrs: []Attribute{
+				{
+					Attribute: netlink.Attribute{
+						Length: 4,
+						Type:   1,
+						Data:   make([]byte, 0),
+					},
+					NetByteOrder: true,
+				},
+			},
+			b: []byte{
+				0x04, 0x00,
+				0x01, 0x40, // NetByteOrder bit
+			},
+		},
+		{
+			name: "max type space, length 0",
+			attrs: []Attribute{
+				{
+					Attribute: netlink.Attribute{
+						Length: 4,
+						Type:   16383,
+						Data:   make([]byte, 0),
+					},
+					Nested:       false,
+					NetByteOrder: false,
+				},
+			},
+			b: []byte{
+				0x04, 0x00,
+				0xFF, 0x3F, // 14 lowest type bits up
+			},
+		},
+		{
+			name: "multiple nested attributes",
+			attrs: []Attribute{
+				{
+					Attribute: netlink.Attribute{
+						Length: 44,
+						Type:   123,
+						Data: []byte{
+							0x14, 0x00, // Depth 1,1
+							0x00, 0x80, // Nested bit
+							0x08, 0x00, // Depth 2,1
+							0x00, 0x00,
+							0x04, 0x03, 0x02, 0x01,
+							0x08, 0x00, // Depth 2,2
+							0x00, 0x00,
+							0x09, 0x08, 0x07, 0x06,
+							0x14, 0x00, // Depth 1,2
+							0x00, 0x00,
+							0x0F, 0x0E, 0x0D, 0x0C,
+							0x0B, 0x0A, 0x09, 0x08,
+							0x07, 0x06, 0x05, 0x04,
+							0x03, 0x02, 0x01, 0x00,
+						},
+					},
+					Nested: true,
+					Children: []Attribute{
+						{
+							Attribute: netlink.Attribute{
+								Length: 20,
+								Type:   0,
+								Data: []byte{
+									0x08, 0x00, // Depth 2,1
+									0x00, 0x00,
+									0x04, 0x03, 0x02, 0x01,
+									0x08, 0x00, // Depth 2,2
+									0x00, 0x00,
+									0x09, 0x08, 0x07, 0x06,
+								},
+							},
+							Nested: true,
+							Children: []Attribute{
+								{
+									Attribute: netlink.Attribute{
+										Length: 8,
+										Type:   0,
+										Data: []byte{
+											0x04, 0x03, 0x02, 0x01,
+										},
+									},
+								},
+								{
+									Attribute: netlink.Attribute{
+										Length: 8,
+										Type:   0,
+										Data: []byte{
+											0x09, 0x08, 0x07, 0x06,
+										},
+									},
+								},
+							},
+						},
+						{
+							Attribute: netlink.Attribute{
+								Length: 20,
+								Type:   0,
+								Data: []byte{
+									0x0F, 0x0E, 0x0D, 0x0C,
+									0x0B, 0x0A, 0x09, 0x08,
+									0x07, 0x06, 0x05, 0x04,
+									0x03, 0x02, 0x01, 0x00,
+								},
+							},
+						},
+					},
+				},
+			},
+			b: []byte{
+				0x2C, 0x00, // Root level
+				0x7B, 0x80, // type 123, Nested bit
+				0x14, 0x00, // Depth 1,1
+				0x00, 0x80,
+				0x08, 0x00, // Depth 2,1
+				0x00, 0x00,
+				0x04, 0x03, 0x02, 0x01,
+				0x08, 0x00, // Depth 2,2
+				0x00, 0x00,
+				0x09, 0x08, 0x07, 0x06,
+				0x14, 0x00, // Depth 1,2
+				0x00, 0x00,
+				0x0F, 0x0E, 0x0D, 0x0C,
+				0x0B, 0x0A, 0x09, 0x08,
+				0x07, 0x06, 0x05, 0x04,
+				0x03, 0x02, 0x01, 0x00,
+			},
+		},
 	}
 
-	msg := netlink.Message{Data: []byte{255, 1, 2, 0}}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			b, err := MarshalAttributes(tt.attrs)
 
-	var gotUnmarshal Header
-	gotMarshal := netlink.Message{Data: []byte{0, 0, 0, 0}}
+			if want, got := tt.err, err; want != got {
+				t.Fatalf("unexpected error:\n- want: %v\n-  got: %v",
+					want, got)
+			}
+			if err != nil {
+				return
+			}
 
-	// Unmarshal msg into gotUnmarshal
-	if err := gotUnmarshal.UnmarshalMessage(msg); err != nil {
-		t.Fatalf("failed to unmarshal message: %v", msg)
-	}
-
-	if want, got := hdr, gotUnmarshal; want != got {
-		t.Fatalf("unexpected unmarshalled Netfilter header:\n- want: %v\n- got: %v\n", want, got)
-	}
-
-	// Re-marshal gotUnmarshal into gotMarshal
-	if err := gotUnmarshal.MarshalMessage(&gotMarshal); err != nil {
-		t.Fatalf("failed to re-marshal message: %v", gotUnmarshal)
-	}
-
-	if want, got := msg, gotMarshal; !reflect.DeepEqual(want, got) {
-		t.Fatalf("unexpected re-marshalled output:\n- want: %v\n- got: %v\n", want, got)
+			if want, got := tt.b, b; !bytes.Equal(want, got) {
+				t.Fatalf("unexpected bytes:\n- want: [%# x]\n-  got: [%# x]",
+					want, got)
+			}
+		})
 	}
 }
 
-func TestAttribute_MarshalUnmarshalAttributes(t *testing.T) {
+func TestAttribute_MarshalTwoWay(t *testing.T) {
 	tests := []struct {
 		name  string
 		attrs []Attribute
--- header_test.go
+++ header_test.go
@@ -7,6 +7,37 @@ import (
 	"github.com/mdlayher/netlink"
 )
 
+func TestHeader_MarshalUnmarshalMessage(t *testing.T) {
+	hdr := Header{
+		Family:     255,
+		Version:    1,
+		ResourceID: 2,
+	}
+
+	msg := netlink.Message{Data: []byte{255, 1, 2, 0}}
+
+	var gotUnmarshal Header
+	gotMarshal := netlink.Message{Data: []byte{0, 0, 0, 0}}
+
+	// Unmarshal msg into gotUnmarshal
+	if err := gotUnmarshal.UnmarshalMessage(msg); err != nil {
+		t.Fatalf("failed to unmarshal message: %v", msg)
+	}
+
+	if want, got := hdr, gotUnmarshal; want != got {
+		t.Fatalf("unexpected unmarshalled Netfilter header:\n- want: %v\n- got: %v\n", want, got)
+	}
+
+	// Re-marshal gotUnmarshal into gotMarshal
+	if err := gotUnmarshal.MarshalMessage(&gotMarshal); err != nil {
+		t.Fatalf("failed to re-marshal message: %v", gotUnmarshal)
+	}
+
+	if want, got := msg, gotMarshal; !reflect.DeepEqual(want, got) {
+		t.Fatalf("unexpected re-marshalled output:\n- want: %v\n- got: %v\n", want, got)
+	}
+}
+
 func TestHeaderType_MarshalUnmarshalNetlink(t *testing.T) {
 	nlht := netlink.HeaderType(0x087B) // 0000 1000 0111 1011
 	nfht := HeaderType{
