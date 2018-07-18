c3c754f5bfb28f16460773c72d4c31052ad3620b
Timo Beckers <timo@incline.eu>
2018-07-18 18:56:48 +0200

    header.go - improve godoc for HeaderType

--- header.go
+++ header.go
@@ -6,8 +6,14 @@ import (
 	"github.com/mdlayher/netlink"
 )
 
-// HeaderType is a Netfilter-specific representation of a Netlink header.
-// Netfilter splits this field in two; Subsystem ID and Message Type.
+// HeaderType is a field found in the Netlink header, but Netfilter subsystems divide
+// this uint16 into two bytes: the most significant byte is the subsystem ID and the
+// least significant is the message type.
+//
+// The significance of the MessageType field is fully dependent on the subsystem
+// the message is for. This package only splits the field into two bytes and provides
+// a list of known SubsystemIDs. Subpackages (for subsystems like Conntrack) then make
+// further use of the MessageType fields to interpret messages accordingly.
 type HeaderType struct {
 	SubsystemID SubsystemID
 	MessageType MessageType
@@ -25,18 +31,21 @@ func UnmarshalNetlinkHeaderType(nlht netlink.HeaderType) HeaderType {
 }
 
 // MarshalNetlinkHeaderType marshals a netfilter.HeaderType into a netlink.HeaderType.
+// It joins the SubsystemID and MessageType fields back together into a uint16 to be placed
+// into the Netlink header.
 func MarshalNetlinkHeaderType(ht HeaderType) netlink.HeaderType {
 	return netlink.HeaderType(uint16(ht.SubsystemID)<<8 | uint16(ht.MessageType))
 }
 
-// Parse the SubsystemID and Message Type to give correct
-// string representation of the Header Type in Netfilter context.
+// String representation of the HeaderType in Netfilter context, for debugging purposes.
 func (ht HeaderType) String() string {
 	return fmt.Sprintf("%s|%d", ht.SubsystemID, ht.MessageType)
 }
 
 // UnmarshalMessage unmarshals the first 4 bytes of a netlink.Message into a netfilter.Header.
+// It safely calls Header.UnmarshalBinary with the correct offset on the Netlink message's Data field.
 func (h *Header) UnmarshalMessage(msg netlink.Message) error {
+
 	err := h.UnmarshalBinary(msg.Data[:nfHeaderLen])
 	if err != nil {
 		return err
@@ -45,8 +54,9 @@ func (h *Header) UnmarshalMessage(msg netlink.Message) error {
 	return nil
 }
 
-// MarshalMessage marshals a nefilter.Header into the first 4 bytes of a netlink.Message's Data attribute.
-func (h Header) MarshalMessage(msg *netlink.Message) error {
+// MarshalMessage safely marshals a netfilter.Header into the correct offset of a netlink.Message's Data field.
+func (h *Header) MarshalMessage(msg *netlink.Message) error {
+
 	hb, err := h.MarshalBinary()
 	if err != nil {
 		return err
