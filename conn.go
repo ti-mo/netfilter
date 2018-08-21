bb0c8ab80c169286d0378ca402c5084c966e1746
Timo Beckers <timo@incline.eu>
2018-08-21 19:58:15 +0200

    conn - Netfilter multicast group listener implementation
    - isMulticast flag on netfilter.Conn
    - allow each Conn to be called Listen() on once
    - JoinGroups() and LeaveGroups()
    - Remove duplicate implementation of netlink.Execute()

--- conn.go
+++ conn.go
@@ -7,6 +7,10 @@ import (
 // Conn represents a Netlink connection to the Netfilter subsystem.
 type Conn struct {
 	conn *netlink.Conn
+
+	// Marks the Conn as being attached to one or more multicast groups,
+	// it can no longer be used for any queries for its remaining lifetime.
+	isMulticast bool
 }
 
 // Open opens a new Netlink connection to the Netfilter subsystem
@@ -28,23 +32,54 @@ func (c *Conn) Close() error {
 	return c.conn.Close()
 }
 
-// Query sends a Netfilter message over Netlink, expects
-// and validates a response and returns the result.
+// Query sends a Netfilter message over Netlink and validates the response.
+// The call will fail if the Conn is marked as Multicast.
 func (c *Conn) Query(nlm netlink.Message) ([]netlink.Message, error) {
 
-	req, err := c.conn.Send(nlm)
-	if err != nil {
-		return nil, err
+	if c.isMulticast {
+		return nil, errConnIsMulticast
 	}
 
-	if err := netlink.Validate(nlm, []netlink.Message{req}); err != nil {
-		return nil, err
+	return c.conn.Execute(nlm)
+}
+
+// JoinGroups attaches the Netlink socket to one or more Netfilter multicast groups.
+// Marks the Conn as Multicast, meaning it can no longer be used for any queries.
+func (c *Conn) JoinGroups(groups []NetlinkGroup) error {
+
+	for _, group := range groups {
+		err := c.conn.JoinGroup(uint32(group))
+		if err != nil {
+			return err
+		}
 	}
 
-	resp, err := c.conn.Receive()
-	if err != nil {
-		return nil, err
+	// Mark the Conn as being attached to a multicast group
+	c.isMulticast = true
+
+	return nil
+}
+
+// LeaveGroups detaches the Netlink socket from one or more Netfilter multicast groups.
+// Does not remove the Multicast flag, open a separate Conn for making queries instead.
+func (c *Conn) LeaveGroups(groups []NetlinkGroup) error {
+
+	for _, group := range groups {
+		err := c.conn.LeaveGroup(uint32(group))
+		if err != nil {
+			return err
+		}
 	}
 
-	return resp, nil
+	return nil
+}
+
+// Receive executes a blocking read on the underlying Netlink socket and returns a Message.
+func (c *Conn) Receive() ([]netlink.Message, error) {
+	return c.conn.Receive()
+}
+
+// IsMulticast returns the Conn's Multicast flag. It is set by calling Listen().
+func (c Conn) IsMulticast() bool {
+	return c.isMulticast
 }
--- errors.go
+++ errors.go
@@ -2,6 +2,11 @@ package netfilter
 
 import "errors"
 
+const (
+	errWrapNetlinkUnmarshalAttrs = "error unmarshaling netlink attributes"
+	errWrapNetlinkMarshalAttrs   = "error marshaling netlink attributes"
+)
+
 var (
 	// errInvalidAttributeFlags specifies if an Attribute's flag configuration is invalid.
 	// From a comment in Linux/include/uapi/linux/netlink.h, Nested and NetByteOrder are mutually exclusive.
@@ -10,6 +15,5 @@ var (
 	errShortMessage = errors.New("cannot parse netfilter message because it is too short")
 	errExistingData = errors.New("netlink message already contains Data payload")
 
-	errWrapNetlinkUnmarshalAttrs = "error unmarshaling netlink attributes"
-	errWrapNetlinkMarshalAttrs   = "error marshaling netlink attributes"
+	errConnIsMulticast = errors.New("Conn is attached to one or more multicast groups and can no longer be used for bidirectional traffic")
 )
